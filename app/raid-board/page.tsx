824fa31260b8ff4e161645737b9ff3f91ecfd2c8
Brendan J Hoffman <brendanjhoffman@gmail.com>
2025-08-30 14:11:22 -0700

    style: separate out raids

--- app/raid-board/page.tsx
+++ app/raid-board/page.tsx
@@ -4,7 +4,14 @@ import raid from "@/lib/raid.json";
 export default async function RaidBoard() {
   return (
     <main className="w-full p-8">
-      <Board data={raid} boardId="raid" />
+      {raid.map((raid, index) => (
+        <div key={index} className="mb-8">
+          <h2 className="text-2xl font-bold text-yellow-300 mb-4">
+            {raid.raid}
+          </h2>
+          <Board data={raid.tasks} boardId="raid" />
+        </div>
+      ))}
     </main>
   );
 }
